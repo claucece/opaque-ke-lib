56ffdbce7c4754fcedf0c13715216f2eb2b5c253
Sofía Celi <cherenkovd69@gmail.com>
2025-03-11 15:28:27 +0000

    Better

--- src/lib.rs
+++ src/lib.rs
@@ -28,16 +28,27 @@ mod opaque_auth {
         ClientRegistration::<DefaultCipherSuite>::start(client_rng, password)
     }
 
+    // TODO: or should we return &[u8]?
+    pub fn client_registation_start_serialize(
+        registration: ClientRegistrationStartResult<DefaultCipherSuite>,
+    ) -> Vec<u8> {
+        registration.message.serialize().to_vec()
+    }
+
     pub fn client_registration_finish<R: CryptoRng + RngCore>(
         client_rng: &mut R,
         password: &[u8],
         registration: ClientRegistrationStartResult<DefaultCipherSuite>,
-        response: RegistrationResponse<DefaultCipherSuite>,
-        params: ClientRegistrationFinishParameters<DefaultCipherSuite>,
+        response: &[u8],
     ) -> Result<ClientRegistrationFinishResult<DefaultCipherSuite>, ProtocolError> {
-        registration
-            .state
-            .finish(client_rng, password, response, params)
+        let response_st = RegistrationResponse::deserialize(response)?;
+
+        registration.state.finish(
+            client_rng,
+            password,
+            response_st,
+            ClientRegistrationFinishParameters::default(),
+        )
     }
 
     pub fn client_login_start<R: CryptoRng + RngCore>(
@@ -47,17 +58,29 @@ mod opaque_auth {
         ClientLogin::<DefaultCipherSuite>::start(client_rng, password)
     }
 
+    // TODO: or should we return &[u8]?
+    pub fn client_login_start_serialize(
+        login: ClientLoginStartResult<DefaultCipherSuite>,
+    ) -> Vec<u8> {
+        login.message.serialize().to_vec()
+    }
+
     pub fn client_login_finish(
         password: &[u8],
         login: ClientLoginStartResult<DefaultCipherSuite>,
-        response: CredentialResponse<DefaultCipherSuite>,
-        params: ClientLoginFinishParameters<DefaultCipherSuite>,
+        response: &[u8],
     ) -> Result<ClientLoginFinishResult<DefaultCipherSuite>, ProtocolError> {
-        login.state.finish(password, response, params)
+        let response_st = CredentialResponse::deserialize(response)?;
+        login.state.finish(
+            password,
+            response_st,
+            ClientLoginFinishParameters::default(),
+        )
     }
 }
 
 pub use opaque_auth::{
-    client_login_finish, client_login_start, client_registration_finish, client_registration_start,
+    client_login_finish, client_login_start, client_login_start_serialize,
+    client_registation_start_serialize, client_registration_finish, client_registration_start,
     DefaultCipherSuite,
 };
